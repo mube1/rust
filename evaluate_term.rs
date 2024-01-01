b3562d64ad3a2000e02b68f136af4b851473e1c7
Mubarek Mohammed <38358880+mube1@users.noreply.github.com>
2024-01-01 23:55:02 +0300

    Update and rename multiplication_parsing.rs to evaluate_term.rs

---
+++ evaluate_term.rs
@@ -0,0 +1,32 @@
+
+fn multiplication_term_evaluate(term:String,_x:Vec<i32>)->i32{
+
+    let mut result : i32= 1;
+    let mut cond=false;
+    let mut counter=0;
+    for ch in term.chars(){
+    counter+=1;
+
+        if cond{            cond=false;         result*=_x[(ch as i32-48) as usize-1 ] ;            continue;
+        }
+        
+        if  ch as i32 >=48 && ch as i32 <=58{          result*=ch as i32-48 ;        }
+        else{    
+            if ch=='/'{
+                return result/multiplication_term_evaluate(term.to_string()[counter..term.len()-1].to_string(),_x);
+            }
+            if  ch != '*' {cond=true;}
+            
+        }
+        
+    }
+
+    result
+    
+}
+
+
+fn main(){
+    println!("{}",evaluate_term("x1*x2*x3/1*x1".to_string(),[2,2,1].to_vec()));
+}
+
